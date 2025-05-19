167036320373ab56c6212b1458a6715fcc03851d
Automatic28m <phanlop.auto@gmail.com>
2025-05-19 20:55:42 +0700

    import fs, path

--- my-portfolio/backend/database.js
+++ my-portfolio/backend/database.js
@@ -3,6 +3,9 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import fs from 'fs';
+import path from 'path';
+
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
