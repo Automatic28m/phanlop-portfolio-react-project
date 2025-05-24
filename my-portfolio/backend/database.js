c45c85ea28889f73534a0e4968ec6113430acc42
Automatic28m <phanlop.auto@gmail.com>
2025-05-24 22:16:49 +0700

    add port to db connection

--- my-portfolio/backend/database.js
+++ my-portfolio/backend/database.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    // port: process.env.MYSQL_PORT,
+    port: process.env.MYSQL_PORT,
     ssl: {
         ca: fs.readFileSync(path.join(__dirname, 'certs', 'ca.pem')),
     },
